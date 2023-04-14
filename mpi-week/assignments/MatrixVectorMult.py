06c764c1b207d2c5721d15da5f985a09e4a2699c
Mouradez <Mourad.EZZEBDI@um6p.ma>
2023-04-14 23:42:55 +0000

    Done

--- mpi-week/assignments/MatrixVectorMult.py
+++ mpi-week/assignments/MatrixVectorMult.py
@@ -28,15 +28,20 @@ def matrixVectorMult(A, b, x):
         for j in range(col):
             x[i] += a[j] * b[j]
 
-    return x
+    return 0
 
 ########################initialize matrix A and vector b ######################
 #matrix sizes
 SIZE = 1000
-Local_size = SIZE // nbOfproc
+Local_size = SIZE//nbOfproc
 
 # counts = block of each proc
-counts = [Local_size] * nbOfproc
+counts = []
+displ =[]
+for i in range(nbOfproc):
+    counts[i] = Local_size 
+    displ[i] = i*local_size 
+# displacements = [i*Local_size for i in range(nbOfproc)]
 
 if RANK == 0:
     A = lil_matrix((SIZE, SIZE))
@@ -51,14 +56,12 @@ else :
 
 
 
-#########Send b to all procs and scatter A (each proc has its own local matrix#####
-LocalMatrix = np.zeros((Local_size, Local_size)) 
-Localb = np.zeros(Local_size)
+b = COMM.bcast(b, root=0)
+LocalMatrix = np.zeros((Local_size, SIZE))
 # Scatter the matrix A
-displacements = [sum(counts[:i]) for i in range(nbOfproc)]
-COMM.Scatterv([A,counts,displacements,MPI.DOUBLE],LocalMatrix,root =0)
+COMM.Scatterv([A, counts, displacements, MPI.DOUBLE], LocalMatrix, root=0)
 
-#####################Compute A*b locally#######################################
+#                                                   Compute A*b
 LocalX = np.zeros(Local_size)
 
 start = MPI.Wtime()
@@ -67,22 +70,16 @@ stop = MPI.Wtime()
 if RANK == 0:
     print("CPU time of parallel multiplication is ", (stop - start)*1000)
 
-##################Gather te results ###########################################
-# sendcouns = local size of result
-sendcounts = [Local_size] * nbOfproc
-sendcounts[-1] += SIZE * nbOfproc
-if RANK == 0: 
-     X = np.zeros(SIZE)
-else :
-     X = None
+#                                                The use of GatherV into X
 
-# Gather the result into X
-COMM.Gatherv(LocalX,[X,sendcounts,displacements, MPI.DOUBLE],root = 0)
+sendcounts = counts[RANK]
+if RANK == 0:
+    X = np.zeros(SIZE, dtype=np.float64)
+else:
+    X = None
 
-##################Print the results ###########################################
+COMM.Gatherv(LocalX, [X, sendcounts, np.arange(nbOfproc, dtype=np.int32) * local_size, MPI.DOUBLE], root=0)
 
 if RANK == 0 :
     X_ = A.dot(b)
     print("The result of A*b using dot is :", np.max(X_ - X))
-    print("The result of A*b using parallel version is :", X)
-    
